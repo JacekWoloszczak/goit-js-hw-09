e892cb08d4374d0f6914b37593e0e181a33ac902
JacekWoloszczak <jacek.woloszczak@gmail.com>
2023-10-11 12:01:59 +0200

    js update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector('body');
+console.log(body);
+const startBtn = document.querySelector('button[data-start]');
+console.log(startBtn);
+startBtn.style.backgroundColor = getRandomHexColor;
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(stopBtn.dataset);
 
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(event => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${getRandomHexColor()} has stopped!`);
+});
